cb05aa8008241838d5cb4c2dc7c34166e421ae3e
1e10laptop <sample@bths.edu>
2022-11-04 12:30:24 -0400

    draft

---
+++ src/Timer.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Timer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What number do you want to count up to? ");
+        int countTo = scanner.nextInt();
+
+        int counter = 1;
+        System.out.println(counter);
+        while (counter < countTo) {
+            counter++;
+            System.out.println(counter);
+        }
+    }
+}
+
